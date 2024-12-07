cc9a53aeb2e1b0ad12eaad8cc918ae474b5cd365
Rizwan <syedrizwanukhaq986@gmail.com>
2024-12-07 10:09:26 +0530

    changed logo and images

--- app/index.jsx
+++ app/index.jsx
@@ -14,7 +14,7 @@ export default function App() {
         <View className='w-full justify-center items-center min-h-[84vh] px-4' >
           <Image 
             source={images.logo}
-            className="w-[130px] h-[84px]"
+            className="w-[180px] h-[84px]"
             resizeMode='contain'
           />
           <Image 
